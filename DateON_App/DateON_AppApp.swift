8123694de124d227eb61ebaf11b115e62a504433
Sanket Mule <sanketmu@cybage.com>
2025-01-02 14:22:08 +0530

    Initial Commit

---
+++ DateON_App/DateON_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateON_AppApp.swift
+//  DateON_App
+//
+//  Created by Sanket Mule on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DateON_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DateON_AppTests/DateON_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DateON_AppTests.swift
+//  DateON_AppTests
+//
+//  Created by Sanket Mule on 02/01/25.
+//
+
+import Testing
+@testable import DateON_App
+
+struct DateON_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
